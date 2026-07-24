67c74cf4740c7fcdf083c14860355bf490f55526
NavyaArora24 <aroranavya78@gmail.com>
2026-07-24 19:21:24 +0530

    OOps started

--- jsadvance/11ThisKey/3script.js
+++ jsadvance/11ThisKey/3script.js
@@ -3,33 +3,39 @@ let username = document.querySelector("#name");
 let role = document.querySelector("#role");
 let bio = document.querySelector("#bio");
 let photo = document.querySelector("#photo");
-let userContainer = document.querySelector('#userContainer');
+let userContainer = document.querySelector("#userContainer");
 
 const userManager = {
-  users: [], //that will store all the users
+  users: [],
+
   init: function () {
-    form.addEventListener("submit", this.submitForm.bind(this)); //this will make obj as this instead of form because when we create function this  was reffering to form instead of obj
+    form.addEventListener("submit", this.submitForm.bind(this));
   },
-  //three methodws created
+
   submitForm: function (e) {
     e.preventDefault();
     this.addUser();
   },
-  addUser: function (e) {
+
+  addUser: function () {
     this.users.push({
-      //agar dono side pe same chiz likhi ho name: name so instead u can write=== name, bio, role
       username: username.value,
       role: role.value,
       bio: bio.value,
       photo: photo.value,
     });
+
     form.reset();
     this.renderUI();
   },
+
   renderUI: function () {
-    const card = document.createElement("div");
-    this.users.forEach(function (user) {
-      userContainer.innerHTML = "";
+    // Clear old cards
+    userContainer.innerHTML = "";
+
+    this.users.forEach((user, index) => {
+      const card = document.createElement("div");
+
       card.className =
         "bg-[#232323] rounded-xl border border-gray-700 p-6 text-center hover:-translate-y-2 hover:shadow-xl transition duration-300";
 
@@ -54,19 +60,25 @@ const userManager = {
       desc.className = "text-gray-400 text-sm mt-4 leading-6";
       desc.textContent = user.bio;
 
-      // Append elements
+      // Append
       card.appendChild(img);
       card.appendChild(name);
       card.appendChild(role);
       card.appendChild(desc);
-      // Add to the page
+
+      // Click event
+      card.addEventListener("click", () => {
+        this.removeUser(index);
+      });
+
       userContainer.appendChild(card);
-      // or
-      // document.getElementById("container").appendChild(card);
     });
   },
-  removeUser: function () {},
+
+  removeUser: function (index) {
+    this.users.splice(index, 1);
+    this.renderUI();
+  },
 };
 
 userManager.init();
-//removeUser by yourself
---
+++ jsadvance/12OOps/script.js
@@ -0,0 +1,50 @@
+//at the place of this a blank object created by new comes and stores the value in there
+//classes me this ki value blank object hoti h jb hm use new word ke sath call krte h--studied in this key lec1
+
+
+function CreateBiscuits(){
+    this.name = "ParleG";
+    this.price = 5;
+    this.qty = 8;
+    this.company = "Parle";
+    this.category = "regular";
+}
+
+let biscuit1 = new CreateBiscuits();
+//biscuit1 is an object filled with thingse that are there in createBiscuits
+
+
+//humein seekhna h factories banaane, matlab ki aap ek bar blueprint bana do ki har object kaisa dikhega and hum log naye naye object with different values bna paenge, this is the sar of what oops does
+
+
+function CreateBiscuits2(name, price, qty, company, category){
+    this.name = name;
+    this.price = price;
+    this.qty = qty;
+    this.company = company;
+    this.category = category;
+}
+
+let biscuit2 = new CreateBiscuits2("Oreo", 10, 5, "Cadbury", "Choclate");
+
+let biscuit3 = new CreateBiscuits2("Fantasy", 20, 3, "Parle", "Choclate");
+
+function CreatePencil(name, price, color){
+    this.name = name, 
+    this.price = price, 
+    this.color = color
+    let h1 = document.createElement("h1");
+    h1.textContent = "Hey How are u??"
+    h1.style.color = color;
+    document.body.append(h1);
+}
+
+//agar tumhara constructor function koi field apne prototype pr attach karle to us constructor se banne waale sabhi new instances yaani ki object ke pas wo field automatically chali jati h
+
+CreatePencil.prototype.company = "Sheriyans";//when some value is same for all objects no need to write it in constructor function as it takes extra space instead add it in the constructor functions prototype
+
+
+//function that run though new are called --- constructor function--------
+//constructor function ka pehla letter capital hona chahiye -good practice
+let pencil1 = new CreatePencil("Natraj", 10, "black")
+let pencil2 = new CreatePencil("DOMS", 15, "red")
